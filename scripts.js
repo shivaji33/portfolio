3c1ea7b3530a11ffd95f6dda93f3fe4bfd8eee24
naidu <naidupilla84@gmail.com>
2022-07-19 15:31:09 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,54 @@
+AOS.init({
+    duration: 1000,
+});
+
+const lightMode = document.getElementById('light-mode-btn');
+
+lightMode.addEventListener('click', () => {
+   const isLightMode =  document.body.classList.contains('light-mode');
+    if (isLightMode) {
+        document.body.classList.remove('light-mode');   
+        lightMode.innerHTML = ' <i class="fa-solid fa-moon"></i>';
+    } else {  
+        document.body.classList.add('light-mode');  
+        lightMode.innerHTML = ' <i class="fa-solid fa-sun"></i>';
+    }
+})
+
+
+const typedText = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+const textArray = ['Software Developer',"Web Developer", 'MEAN Stack', 'Tech Enthusiast', 'Telugu Guy'];
+charIndex = 0;
+textArrayIndex = 0;
+
+function type() {
+    if (charIndex <= textArray[textArrayIndex].length - 1) {
+      cursor.classList.remove("blink");
+      typedText.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 120);
+    } else {
+      cursor.classList.add("blink");
+      setTimeout(erase, 1000);
+    }
+  }
+  
+  function erase() {
+    if (charIndex > 0) {
+      cursor.classList.remove("blink");
+      typedText.textContent = textArray[textArrayIndex].slice(0, charIndex - 1);
+      charIndex--;
+      setTimeout(erase, 80);
+    } else {
+      cursor.classList.add("blink");
+      textArrayIndex++;
+  
+      if (textArrayIndex > textArray.length - 1) {
+        textArrayIndex = 0;
+      }
+      setTimeout(type, 1000);
+    }
+  }
+
+  type();
